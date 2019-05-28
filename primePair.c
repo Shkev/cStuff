7ec1392af3608a24f3aa8869356dec9f2a64fe16
Shkev <shkev.az@gmail.com>
2019-05-27 22:26:46 -0700

    adding primePair.c

---
+++ primePair.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+  char input[50];
+  long int k;
+  int mod1;
+  int mod2;
+  int count = 0;
+  printf("What is the max number you would like to check?\n");
+  fgets(input, 10, stdin);
+  sscanf(input, "%ld", &k);
+  for (int j = 3; j < k; j++)
+  {
+    for (int i = 2; i < j; i++)
+    {
+      mod1 = j % i;
+      mod2 = (j + 2) % i;
+      if ((mod1 != 0) & (mod2 != 0))
+      {
+        count++;
+      }
+    }
+    if (count == (j - 2))
+    {
+      printf("%d and %d are a Prime Pair\n", j, j + 2);
+    }
+    count = 0;
+  }
+}
