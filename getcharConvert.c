763f0aca69febddf8f4f89ea8f112ac5851aad07
Shkev <shkev.az@gmail.com>
2019-06-11 23:08:00 -0700

    adding getcharConvert.c

---
+++ getcharConvert.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+/*int main()
+{
+  int x = log2(2);
+  printf("%d\n", x);
+}*/
+void printArr(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    printf("%c", arr[i]);
+    if (i == size)
+    {
+      printf("\n");
+    }
+  }
+}
+
+int makeNum(int arr[], int size)
+{
+  int output;
+
+  return output;
+}
+
+int convertBinary(int input[], int size, int num2[])
+{
+  //printf("%d\n", size);
+  int num10;
+  //num10 = makeNum(input, size);
+  for (int i = size - 1; i > -1; i--)
+  {
+    //printf("%d", i);
+    int x = input[i];
+    printf("%c\n", input[i]);
+    num10 += (x * (10 ^ i));
+    //printf("%d\n", num10);
+  }
+  //printf("\n%d\n", num10);
+  int placeValMax;
+  float store1;
+  int count = 0;
+  store1 = floor(log2(num10));
+  int store = store1;
+  placeValMax = store;
+  while (num10 != 0)
+  {
+    int val1 = placeValMax;
+    placeValMax = floor(log2(num10));
+    int val2 = placeValMax;
+    num10 -= (2 ^ placeValMax);
+    if ((count != 0) & (val2 = val1 - 1))
+    {
+      num2[store - count] = 1;
+    }
+    else
+    {
+      if (val2 != val1 - 1)
+      {
+        num2[store - count] = 0;
+      }
+    }
+    count++;
+  }
+  if (placeValMax != 0)
+  {
+    for (int j = 0; j < store - count; j++)
+    {
+      num2[j] = 0;
+    }
+  }
+  return store - 1;
+}
+
+int main()
+{
+  int ch;
+  int strch[50];
+  ch = getchar();
+  int j = 0;
+  while (ch != '\n')
+  {
+    strch[j] = ch;
+    ch = getchar();
+    j++;
+  }
+  int hexadecimal[25];
+  printf("\nDecimal:\n");
+  printArr(strch, j);
+  int binaryNum[150];
+  printf("\nBinary\n");
+  int f = convertBinary(strch, j, binaryNum);
+  printArr(binaryNum, f);
+  printf("\nHexadecimal\n");
+
+  exit(EXIT_SUCCESS);
+}
