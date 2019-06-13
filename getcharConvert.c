ae16db1cadd1f881161fc12084c671f3ae4c0c22
Shkev <39930235+Shkev@users.noreply.github.com>
2019-06-12 17:16:04 -0700

    Update getcharConvert.c
    project incomplete

--- getcharConvert.c
+++ getcharConvert.c
@@ -1,7 +1,9 @@
+//incomplete
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+
 /*int main()
 {
   int x = log2(2);
@@ -19,26 +21,25 @@ void printArr(int arr[], int size)
   }
 }
 
-int makeNum(int arr[], int size)
+void makeNum(int arr[], int size)
 {
-  int output;
-
-  return output;
+  for (int i = size - 1; i > -1; i--)
+  {
+    int x = arr[i];
+    printf("%c\n", x);
+    for (int j = 0; j < i - 1; j++)
+    {
+      arr[i] = (x * 100) + 48;
+    }
+    printf("%d\n", arr[i]);
+  }
 }
 
 int convertBinary(int input[], int size, int num2[])
 {
   //printf("%d\n", size);
   int num10;
-  //num10 = makeNum(input, size);
-  for (int i = size - 1; i > -1; i--)
-  {
-    //printf("%d", i);
-    int x = input[i];
-    printf("%c\n", input[i]);
-    num10 += (x * (10 ^ i));
-    //printf("%d\n", num10);
-  }
+  makeNum(input, size); 
   //printf("\n%d\n", num10);
   int placeValMax;
   float store1;
@@ -54,13 +55,17 @@ int convertBinary(int input[], int size, int num2[])
     num10 -= (2 ^ placeValMax);
     if ((count != 0) & (val2 = val1 - 1))
     {
-      num2[store - count] = 1;
+      num2[store - count - 1] = 1;
     }
     else
     {
       if (val2 != val1 - 1)
       {
-        num2[store - count] = 0;
+        int dif = val1 - val2;
+        for (int j = 1; j <= dif; j++)
+        {
+        num2[store - count - j] = 0;
+        }
       }
     }
     count++;
@@ -72,7 +77,7 @@ int convertBinary(int input[], int size, int num2[])
       num2[j] = 0;
     }
   }
-  return store - 1;
+  return store + 1;
 }
 
 int main()
