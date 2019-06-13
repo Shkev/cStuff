aa477e1dd26d8a6646d7c9c587c79a8010ab944f
Shkev <39930235+Shkev@users.noreply.github.com>
2019-06-12 20:35:06 -0700

    Update mentalSums.c

--- Competitions/PROCO/mentalSums.c
+++ Competitions/PROCO/mentalSums.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 int main()
@@ -7,14 +8,15 @@ int main()
   FILE* input;
   FILE* output;
   int Q, a, b;
-  input = fopen("./standard_input.txt", "r");
-  output = fopen("./standard_output.txt", "w");
+  input = fopen("standard input", "r");
+  output = fopen("standard output", "w");
   fscanf(input, "%d %d %d", &Q, &a, &b);
   //printf("%d %d %d\n", Q, a, b);
   int mod = pow(10, 5) + 7;
   //printf("%d\n", mod);
   int xArr[Q];
   int count[Q];
+  memset(count, 0, 25);
   int num = 0;
   for (int i = 0; i >= 0; i++)
   {
@@ -24,28 +26,28 @@ int main()
     } 
     int j = i % Q;
     xArr[j] = ((a + (b * (j + 1))) % mod);
+    //printf("%d\n", xArr[j]);
     int sum = 0;
     while (xArr[j] != 0)
     {
-      if ((xArr[j] / 10) < 1)
+      if ((((xArr[j] / 10) < 1))
       {
         num++;
         break;
       }
       int digit = xArr[j] % 10;
       sum += pow(digit, 2);
+      //printf("%d\n", sum);
       xArr[j] /= 10;
       count[j]++; 
     }
     xArr[j] = sum;
+    printf("%d %d\n", xArr[j], num);
   }
   for (int i = 0; i < Q; i++)
   {
-    fprintf(output, "%d\n", xArr[i]);
+    printf("%d\n", count[i]);
   }
-  //int c, d, e;
-  //fscanf(output, "%d\n%d\n%d", &c, &d, &e);
-  //printf("%d %d %d", c, d, e);
   fclose(input);
   fclose(output);
   exit(EXIT_SUCCESS);
