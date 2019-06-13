9cfb01257ee69399dce59000475177a12e3290d4
Shkev <shkev.az@gmail.com>
2019-06-12 23:23:03 -0700

    changing mentalSums.c, adding input and output files

--- Competitions/PROCO/mentalSums.c
+++ Competitions/PROCO/mentalSums.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#define TRUE       0
+#define FALSE      1
 
 int main()
 {
   FILE* input;
   FILE* output;
   int Q, a, b;
-  input = fopen("standard input", "r");
-  output = fopen("standard output", "w");
+  input = fopen("./standard_input.txt", "r");
+  output = fopen("./standard_output.txt", "w");
   fscanf(input, "%d %d %d", &Q, &a, &b);
   //printf("%d %d %d\n", Q, a, b);
   int mod = pow(10, 5) + 7;
@@ -17,36 +19,53 @@ int main()
   int xArr[Q];
   int count[Q];
   memset(count, 0, 25);
-  int num = 0;
+  int sum = 0;
+  int single = FALSE;
   for (int i = 0; i >= 0; i++)
   {
-    if (num == Q)
+    int num = 0;
+    int j = i % Q;
+    if (i < Q)
+    {
+      xArr[j] = ((a + (b * (j + 1))) % mod);
+    }
+    //printf("%d\n ", xArr[j]);
+    for (int k = 0; k < Q; k++)
+    {
+      num += (xArr[j]/ 10);
+    }   
+    if (num == 0)
     {
       break;
-    } 
-    int j = i % Q;
-    xArr[j] = ((a + (b * (j + 1))) % mod);
-    //printf("%d\n", xArr[j]);
-    int sum = 0;
+    }
+    if ((xArr[j] / 10) < 1)
+    {
+      single = TRUE;
+      continue;
+    }
+    sum = 0;
+    //loop to separate digits
     while (xArr[j] != 0)
     {
-      if ((((xArr[j] / 10) < 1))
+      if ((single == TRUE) & ((xArr[j] / 10) < 1))
       {
-        num++;
         break;
       }
       int digit = xArr[j] % 10;
       sum += pow(digit, 2);
-      //printf("%d\n", sum);
       xArr[j] /= 10;
-      count[j]++; 
+      printf("%d\n", xArr[j]);
+      if (xArr[j] != 0)
+      {
+        count[j]++;
+      }
     }
     xArr[j] = sum;
-    printf("%d %d\n", xArr[j], num);
+    printf("%d\n", xArr[j]);
   }
   for (int i = 0; i < Q; i++)
   {
-    printf("%d\n", count[i]);
+    fprintf(output, "%d\n", count[i]);
   }
   fclose(input);
   fclose(output);
