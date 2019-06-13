a05a7824bf0359f473d2274613514ab26b54b613
Shkev <shkev.az@gmail.com>
2019-06-13 16:14:26 -0700

    cleaning up code for mentalSums.c

--- Competitions/PROCO/mentalSums.c
+++ Competitions/PROCO/mentalSums.c
@@ -10,8 +10,8 @@ int main()
   FILE* input;
   FILE* output;
   int Q, a, b;
-  input = fopen("standard input", "r");
-  output = fopen("standard output", "w");
+  input = fopen("standard_input.txt", "r");
+  output = fopen("standard_output.txt", "w");
   fscanf(input, "%d %d %d", &Q, &a, &b);
   int mod = pow(10, 5) + 7;
   int xArr[Q];
@@ -23,13 +23,10 @@ int main()
   {     
     sum[i] = 100;
     count[i] = 0;
-    printf("i: %d\n", i);
     xArr[i] = ((a + (b * (i + 1))) % mod);
     //printf("%d\n", xArr[i]);
     while ((sum[i] / 10) != 0)
-    {
-      int j = k % Q; 
-      printf("arr: %d\n", xArr[i]);
+    { 
       if (((xArr[i] / 10) < 1) & (count[i] == 0))
       {
         single = TRUE;
@@ -39,7 +36,6 @@ int main()
         single = FALSE;
       }
       sum[i] = 0;
-      numDigit = 0;
       //loop to separate digits
       while (xArr[i] != 0)
       {
@@ -47,15 +43,12 @@ int main()
         {
           count[i] = 0;
           sum[i] = 0;
-          numDigit = 1;
           break;
         }
         int digit = xArr[i] % 10;
-        numDigit++;
         sum[i] += pow(digit, 2);
         xArr[i] /= 10;
       }
-      printf("numDigit: %d\n", numDigit);
       if (single == FALSE)
       {
         count[i]++;
