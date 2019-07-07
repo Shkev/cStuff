cfa1792900df72407a119da9fa40824f94b2214d
Shkev <shkev.az@gmail.com>
2019-07-07 14:36:03 -0700

    changing primePair.c

--- cBook/Chapter1/primePair.c
+++ cBook/Chapter1/primePair.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
-int main()
+void primePair(long int max_val)
 {
-  char input[50];
-  long int k;
   int mod1;
   int mod2;
   int count = 0;
-  printf("What is the max number you would like to check?\n");
-  fgets(input, 10, stdin);
-  sscanf(input, "%ld", &k);
-  for (int j = 3; j < k; j++)
+  for (int j = 3; j < max_val; j++)
   {
     for (int i = 2; i < j; i++)
     {
@@ -27,4 +22,34 @@ int main()
     }
     count = 0;
   }
+}
+
+int main()
+{
+  char input[50];
+  long int k;
+  //int mod1;
+  //int mod2;
+  //int count = 0;
+  printf("What is the max number you would like to check?\n");
+  fgets(input, 10, stdin);
+  sscanf(input, "%ld", &k);
+  /*for (int j = 3; j < k; j++)
+  {
+    for (int i = 2; i < j; i++)
+    {
+      mod1 = j % i;
+      mod2 = (j + 2) % i;
+      if ((mod1 != 0) & (mod2 != 0))
+      {
+        count++;
+      }
+    }
+    if (count == (j - 2))
+    {
+      printf("%d and %d are a Prime Pair\n", j, j + 2);
+    }
+    count = 0;
+  }*/
+  primePair(k);
 }
